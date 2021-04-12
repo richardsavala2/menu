05d1619a343017005b64ce38e087750fc97b875c
richard savala <59234279+thrillhobaby@users.noreply.github.com>
2021-04-11 20:25:02 -0600

    Add files via upload
    in APP.JS added displayMenuItems function, event listener, and sectionCenter variable. 
    
    in INDEX.HTML added title filter buttons, section-center div, and menu-item article.

--- app.js
+++ app.js
@@ -76,7 +76,24 @@ const menu = [{
 const sectionCenter = document.querySelector(".section-center");
 
 window.addEventListener('DOMContentLoaded', function() {
-    let displayMenu = menu.map(function(item) {
-        console.log(item);
-    })
-})
+    displayMenuItems(menu);
+});
+
+function displayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function(item) {
+        // console.log(item);
+
+        return `<article class="menu-item">
+    <img src=${item.img} class="photo" alt="item.title">
+    <div class="item-info">
+        <header>
+            <h4>${item.title}</h4>
+            <h4 class="price">${item.price}</h4>
+        </header>
+        <p>${item.desc}</p>
+    </div>
+</article>`
+    });
+    displayMenu = displayMenu.join('');
+    sectionCenter.innerHTML = displayMenu;
+}
